68ec1f3d92518305d6a76acf7621ab98f6f94983
gommjabbar <gommjabbar@gmail.com>
2015-05-20 17:08:15 +0300

    Extension methods

---
+++ ActivityTrack.Business/CodeExamples/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityTrack.Business
+{
+    public static class ExtensionMethods
+    {
+        public static string ToExt(this object obj)
+        {
+            return "Ext";
+        }
+
+        public ExtensionMethods()
+        {
+            var o = new Object();
+            var res = o.ToExt();
+        }
+    }
+}
