05cf41abdbc47543786431814376fa412728f9d7
orhamarius <orha.marius@gmail.com>
2015-05-21 21:05:47 +0300

    Minimal modifications for Activity and IdentityModels models

--- ActivityTrack/Models/Activity.cs
+++ ActivityTrack/Models/Activity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
@@ -10,13 +11,21 @@ namespace ActivityTrack.Models
     public class Activity
     {
         public int Id { get; set; }
+
+        [Display(Name="Start Date")]
         public DateTime StartDate { get; set; }
+
+        [Display(Name = "End Date")]
         public DateTime EndDate { get; set; }
+
+        [Display(Name = "Description")]
         public string ActivityDescription { get; set; }
 
+        [Display(Name = "Type")]
         public int ActivtyTypeId { get; set; }
         public virtual ActivityType ActivityType { get; set; }
 
+        [Display(Name = "Project Id")]
         [ForeignKey("Project")]
         public int ProjectId { get; set; }
 
