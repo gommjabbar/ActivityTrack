fe44ec4bc65bcdb0a4c7cd5be74b83f0e56006ce
SzekelyAlexandru <szekely_aly@yahoo.com>
2015-05-31 00:15:29 +0300

    Filter by ProjectTypeIdD

--- ActivityTrack/Scripts/App/Activity/Controllers/displayActivitiesController.js
+++ ActivityTrack/Scripts/App/Activity/Controllers/displayActivitiesController.js
@@ -1,11 +1,15 @@
 ﻿(function() {
     var app = angular.module("activityTrack");
-    app.controller("displayActivities", ['$scope', 'activityService',
+    app.controller("displayActivitiesController", ['$scope', 'activityService',
 
-         function ($scope, activityService) {
-          
+         function ($scope, activityService){
+             $scope.Activities = [
+   { Type: 1, Name: '1st', StartDate: newDate(), EndDate: newDate(), Descritipn: 'test1' },
+   { Type: 2, Name: '2nd', StartDate: newDate(), EndDate: newDate(), Descritipn: 'test2' },
+   { Type: 3, Name: '3rd', StartDate: newDate(), EndDate: newDate(), Descritipn: 'test3' }
+             ];
 
-             $scope.getActivity = function () {
+             $scope.getActivities = function () {
                  $.ajax(
                  {
                      url: '/api/activities',
