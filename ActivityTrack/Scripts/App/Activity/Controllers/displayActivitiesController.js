c9b07b7267086477ebefb6777d15ff4d41952c45
SzekelyAlexandru <szekely_aly@yahoo.com>
2015-06-05 19:12:11 +0300

    Working index view(list of all activities)

--- ActivityTrack/Scripts/App/Activity/Controllers/displayActivitiesController.js
+++ ActivityTrack/Scripts/App/Activity/Controllers/displayActivitiesController.js
@@ -1,26 +1,26 @@
-﻿(function() {
+﻿(function () {
     var app = angular.module("activityTrack");
     app.controller("displayActivitiesController", ['$scope', 'activityService',
 
          function ($scope, activityService){
-             $scope.Activities = [
-   {
-       Type: 1, Name: '1st', StartDate: new Date(), EndDate: new Date(), Description: 'test1' },
-   { Type: 2, Name: '2nd', StartDate: new Date(), EndDate: new Date(), Description: 'test2' },
-   { Type: 3, Name: '3rd', StartDate: new Date(), EndDate: new Date(), Description: 'test3' }
-             ];
+       $scope.reverse = true;
+       $scope.order = function (predicate) {
+           $scope.reverse = ($scope.predicate === predicate) ? !$scope.reverse : true;
+           $scope.predicate = predicate;
+       };
 
-             $scope.getActivities = function () {
-                 $.ajax(
-                 {
-                     url: '/api/activities',
-                     type: 'get',
-                     data: $scope.activity
-                 }).done(function (data) {
-                     $scope.$apply();
-                 })
-             }
-             
-        }]);
-         //});
+       $scope.getActivities = function () {
+           $.ajax(
+           {
+               url: '/api/activities',
+               type: 'get',
+           }).done(function (data) {
+               $scope.Activities = data;
+               $scope.$apply();
+           })
+       }
+       $scope.getActivities();
+       
+   }]);
+    //});
 })();
