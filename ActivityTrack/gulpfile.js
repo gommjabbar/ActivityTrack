4c0ea8c952a9f6a4605c7e3c47857abf5fa706d7
SzekelyAlexandru <szekely_aly@yahoo.com>
2015-06-10 00:24:29 +0300

    Gulp task: JavaScript hinting

--- ActivityTrack/gulpfile.js
+++ ActivityTrack/gulpfile.js
@@ -3,3 +3,13 @@
 gulp.task('default', function() {
   // place code for your default task here
 });
+
+gulp.task('jshint', function() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(plumber({
+        errorHandler: onError
+    }))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(notify({ message: 'JS Hinting task complete' }));
+});
