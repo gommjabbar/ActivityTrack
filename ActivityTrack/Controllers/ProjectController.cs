2278faeceabd3ff2c889f96a99ba868d66d2af88
orhamarius <orha.marius@gmail.com>
2015-06-24 12:56:53 +0300

    knockout

---
+++ ActivityTrack/Controllers/ProjectController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ActivityTrack.Models;
+
+namespace ActivityTrack.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Project
+        public ActionResult Index()
+        {
+            return View(db.Projects.ToList());
+        }
+
+        // GET: Project/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectEO projectEO = db.Projects.Find(id);
+            if (projectEO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectEO);
+        }
+
+        // GET: Project/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Project/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,CreateDate,UpdateDate,DeleteDate")] ProjectEO projectEO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Projects.Add(projectEO);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(projectEO);
+        }
+
+        // GET: Project/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectEO projectEO = db.Projects.Find(id);
+            if (projectEO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectEO);
+        }
+
+        // POST: Project/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,CreateDate,UpdateDate,DeleteDate")] ProjectEO projectEO)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(projectEO).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(projectEO);
+        }
+
+        // GET: Project/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectEO projectEO = db.Projects.Find(id);
+            if (projectEO == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectEO);
+        }
+
+        // POST: Project/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProjectEO projectEO = db.Projects.Find(id);
+            db.Projects.Remove(projectEO);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
